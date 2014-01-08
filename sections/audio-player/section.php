d5710b64cc5c675a317e5d036b4214010132752d
Aleksander Hansson <hello@ahansson.com>
2014-01-08 23:36:32 +0100

    fix attempt

--- sections/audio-player/section.php
+++ sections/audio-player/section.php
@@ -84,28 +84,28 @@ class AudioPlayer extends PageLinesSection {
 						useOnlyMp3Format: true,
 						/* sound_id: unique string for player identification (if multiple player instances were used, then strings need to be different!) */
 						sound_id: 'sound_id<?php echo $prefix; ?>',
-						
+
 						/* playlistList: dom elements which holds list of playlists */
 						playlistList: '#playlist_list<?php echo $prefix; ?>',
-						/* activePlaylist: set active playlist that will be loaded on beginning. 
+						/* activePlaylist: set active playlist that will be loaded on beginning.
 						param1: hidden (boolean) true/false (visible/hidden playlist)
 						param2: id (pass element 'id' from the dom)
 						Leave empty for no playlist loaded at start like this: activePlaylist: '' */
 						activePlaylist: {hidden: false, id: '#playlist<?php echo $prefix; ?>'},
 						/* activeItem: active item to start with when playlist is loaded (0 = first, 1 = second, 2 = third... -1 = none) */
 						activeItem: 0,
-						
+
 						/* autoOpenPlayerInPopup: true/false. Auto open player in popup (removes player in parent window when player in popup opens) */
 						autoOpenPlayerInPopup: false,
 						/* autoUpdateWindowData: true/false. Auto update data between parent window and popup window (current (last) playlist, active item, last volume) */
 						autoUpdateWindowData: true,
-						
-						/* soundcloudApiKey: If you want to use SoundCloud music, register you own api key here for free: 
+
+						/* soundcloudApiKey: If you want to use SoundCloud music, register you own api key here for free:
 						'http://soundcloud.com/you/apps/new' and enter Client ID */
 						soundcloudApiKey: '<?php echo $soundcloud_api; ?>',
 						/* soundcloud_result_limit: max number of results to retrieve from soundcloud. BEWARE! Some results may contain thousands of songs so keep this in mind!! */
 						soundcloud_result_limit: 50,
-						
+
 						/* podcast_result_limit: max number of results to retrieve from podcast. 250 = max possible results by google api feed. */
 						podcast_result_limit: 5,
 						/* yt_playlist_result_limit: max number of results to retrieve from youtube playlist. 200 = max amount youtube playlist can have. */
@@ -123,7 +123,7 @@ class AudioPlayer extends PageLinesSection {
 						randomPlay:false,
 						/*loopingOn: true/false (loop on the end of the playlist) */
 						loopingOn:true,
-						
+
 						/* usePlaylistRollovers: true/false. Use rollovers on playlist items (mouseenter, mouseleave + callbacks) */
 						usePlaylistRollovers: false,
 						/* playlistItemContent: title/thumb/all. Auto create titles or thumbnails in playlist items, or both. */
@@ -132,31 +132,31 @@ class AudioPlayer extends PageLinesSection {
 						useNumbersInPlaylist: false,
 						/* titleSeparator: String to append between song number and title. */
 						titleSeparator: '.&nbsp;',
-						
+
 						/* autoSetSongTitle: true/false. Auto set song title in 'player_mediaName'. */
 						autoSetSongTitle: true,
-						
+
 						/* useSongNameScroll: true/false. Use song name scrolling. */
 						useSongNameScroll: true,
 						/* scrollSpeed: speed of the scroll (number higher than zero). */
 						scrollSpeed: 1,
 						/* scrollSeparator: String to append between scrolling song name. */
 						scrollSeparator: '&nbsp;&#42;&#42;&#42;&nbsp;',
-						
+
 						/* mediaTimeSeparator: String between current and total song time. */
 						mediaTimeSeparator: '&nbsp;-&nbsp;',
-						
+
 						/* useVolumeTooltip: true/false. use tooltip over volume seekbar */
 						useVolumeTooltip: true,
-						
+
 						/* useSeekbarTooltip: true/false. use tooltip over progress seekbar */
 						useSeekbarTooltip: true,
 						/* seekTooltipSeparator: String between current and total song position, for progress tooltip. */
 						seekTooltipSeparator: '&nbsp;/&nbsp;',
-						
+
 						/* defaultArtistData: Default text for song media name. */
 						defaultArtistData: 'Artist&nbsp;Name&nbsp;-&nbsp;Artist&nbsp;Title',
-						
+
 						/* useBtnRollovers: true/false. Use rollovers on buttons */
 						useBtnRollovers: true,
 						/* buttonsUrl: url of the buttons for normal and rollover state */
@@ -170,20 +170,20 @@ class AudioPlayer extends PageLinesSection {
 									shuffle: "<?php printf('%srandom.png', $buttons_path ); ?>", shuffleOn: "<?php printf('%srandomOn.png', $buttons_path ); ?>"},
 						/* useAlertMessaging: true/false. Alert error messages to user */
 						useAlertMessaging: false,
-						
+
 						/* activatePlaylistScroll: true/false. activate jScrollPane. */
 						activatePlaylistScroll: false,
 						/* playlistScrollOrientation: vertical/horizontal. */
 						playlistScrollOrientation: 'horizontal',
-						
+
 						/* sortablePlaylistItems: true/false. Make playlist items sortable */
 						sortablePlaylistItems: false,
 						/* useRemoveBtnInTracks: true/false. Create remove buttons in playlist items for removing tracks. */
 						useRemoveBtnInTracks: false,
-						
+
 						/* autoReuseMailForDownload: true/false. download backup for ios, save email after client first enters email address and auto send all emails to the same address */
 						autoReuseMailForDownload: true,
-						
+
 						/* useKeyboardNavigation: false/false. Use keyboard navigation for music (space=toggle audio, left arrow=previous media, right arrow=next media, m=toggle volume) */
 						useKeyboardNavigation: false
 					};
@@ -191,16 +191,16 @@ class AudioPlayer extends PageLinesSection {
 					jQuery(document).ready(function() {
 
 						var hap_player1, hap_players = [hap_player1];
-							
+
 							jsReady = true;
-								
+
 							var dataArr = [{holder: jQuery('#componentWrapper<?php echo $prefix; ?>'), settings:ap_settings<?php echo $prefix; ?>}];
-									
-							checkFlash(dataArr);	
-							
+
+							checkFlash(dataArr);
+
 							//init component
 							hap_players[0] = jQuery('#componentWrapper<?php echo $prefix; ?>').html5audio(ap_settings<?php echo $prefix; ?>);
-							
+
 					});
 
 				</script>
